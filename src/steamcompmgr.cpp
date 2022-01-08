688bd218a0b8714b3907d31718544817ab5645fe
Joshua Ashton <joshua@froggi.es>
2022-01-08 06:22:30 +0000

    steamcompmgr: Add some comments to what we are doing in the global focus code

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -2183,6 +2183,7 @@ found:
 	if (global_focus.inputFocusWindow)
 		global_focus.inputFocusMode = global_focus.inputFocusWindow->inputFocusMode;
 
+	// Pick keyboard focus window
 	global_focus.keyboardFocusWindow = global_focus.inputFocusWindow;
 	if ( global_focus.inputFocusMode )
 	{
@@ -2191,6 +2192,7 @@ found:
 			: global_focus.focusWindow;
 	}
 
+	// Tell wlserver about our keyboard/mouse focus.
 	if ( global_focus.inputFocusWindow    != previous_focus.inputFocusWindow ||
 		 global_focus.keyboardFocusWindow != previous_focus.keyboardFocusWindow )
 	{
@@ -2287,6 +2289,7 @@ found:
 		}
 	}
 
+	// Set SDL window title
 	if ( global_focus.focusWindow )
 		sdlwindow_title( global_focus.focusWindow->title );
 }
