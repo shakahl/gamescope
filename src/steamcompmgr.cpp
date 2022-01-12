shakahl/gamescope

commit f51a05b2dcfbae0b807547c2d24c708ce7caddca

steamcompmgr: use poll() to wait for X11 events
References: https://github.com/Plagman/gamescope/issues/201

src/steamcompmgr.cpp
@@ -3284,10 +3284,35 @@ steamcompmgr_main (int argc, char **argv)
 	std::thread imageWaitThread( imageWaitThreadMain );
 	imageWaitThread.detach();
 
+	struct pollfd x11_pollfd = {
+		.fd = XConnectionNumber(dpy),
+		.events = POLLIN,
+	};
+
 	for (;;)
 	{
 		focusDirty = False;
 		bool vblank = false;
+
+		if ( poll( &x11_pollfd, 1, -1 ) < 0)
+		{
+			if ( errno == EAGAIN )
+				continue;
+
+			perror( "poll failed" );
+			break;
+		}
+
+		if ( x11_pollfd.revents & POLLHUP )
+		{
+			fprintf( stderr, "Lost connection to the X11 server\n" );
+			break;
+		}
+
+		if ( !( x11_pollfd.revents & POLLIN ) )
+		{
+			continue;
+		}
 
 		do {
 			XNextEvent (dpy, &ev);

commit dd68d7bffe9f1a6b8811a807abff1d3d4df930cc

steamcompmgr: Track X-dirtying outside of dispatch
If we dirty the X server outside of the dispatch, xlib can flush its events, leading us to a state where XPending(dpy) is true but the poll returns false.

Fixes: f51a05b2dcfbae ("steamcompmgr: use poll() to wait for X11 events")

src/steamcompmgr.cpp
@@ -2103,6 +2103,8 @@ determine_and_apply_focus(xwayland_ctx_t *ctx, std::vector<win*>& vecGlobalPossi
 	}
 
 	XFree(children);
+
+	ctx->dirty_x = true;
 }
 
 wlr_surface *win_surface(win *window)
@@ -4043,6 +4045,7 @@ void init_xwayland_ctx(gamescope_xwayland_server_t *xwayland_server)
 
 	ctx->xwayland_server = xwayland_server;
 	ctx->dpy = xwayland_server->get_xdisplay();
+	ctx->dirty_x = true;
 	if (!ctx->dpy)
 	{
 		xwm_log.errorf("Can't open display");
@@ -4351,6 +4354,16 @@ steamcompmgr_main(int argc, char **argv)
 		focusDirty = false;
 		bool vblank = false;
 
+		{
+			gamescope_xwayland_server_t *server = NULL;
+			for ( size_t i = 0; (server = wlserver_get_xwayland_server(i)); i++ )
+			{
+				assert( server );
+				if ( server->ctx->dirty_x && XPending( server->ctx->dpy ) )
+					dispatch_x11( server->ctx.get() );
+			}
+		}
+
 		if ( poll( pollfds.data(), pollfds.size(), -1 ) < 0)
 		{
 			if ( errno == EAGAIN )
@@ -4374,9 +4387,9 @@ steamcompmgr_main(int argc, char **argv)
 
 		for (size_t i = EVENT_X11; i < pollfds.size(); i++)
 		{
-			if ( pollfds[ i ].revents & POLLIN )
+			gamescope_xwayland_server_t *server = wlserver_get_xwayland_server(i - EVENT_X11);
+			if ( ( pollfds[ i ].revents & POLLIN )  )
 			{
-				gamescope_xwayland_server_t *server = wlserver_get_xwayland_server(i - EVENT_X11);
 				assert(server);
 				dispatch_x11( server->ctx.get() );
 			}
@@ -4397,6 +4410,7 @@ steamcompmgr_main(int argc, char **argv)
 							 (unsigned char *)&inputCounter, 1 );
 
 			lastPublishedInputCounter = inputCounter;
+			root_ctx->dirty_x = true;
 		}
 
 		if (focusDirty == true)

src/xwayland_ctx.hpp
@@ -34,6 +34,7 @@ struct xwayland_ctx_t
 {
 	gamescope_xwayland_server_t *xwayland_server;
 	Display			*dpy;
+	bool			dirty_x;
 
 	win				*list;
 	int				scr;